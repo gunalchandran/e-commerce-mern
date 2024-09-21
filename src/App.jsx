4aa72724d5911a07cff666853ae7dbbbd8f46e4e
Gunal <gunalc.22cse@kongu.edu>
2024-09-21 23:22:54 +0530

    feed:added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navabar/Navbar.jsx'
 import Hero from './components/Navabar/Hero/Hero.jsx'
 import { Category } from './components/category/Category.jsx'
+import { Category2 } from './components/category/Category2.jsx'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <Category/>
+      <Category2/>
     </div>
   )
 }
--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -14,38 +14,39 @@ export const Category = () => {
                                 <div>
                                     <p className='mb-4'>Enjoy</p>
                                     <p className='mb-[2px] text-gray-400 '>with</p>
-                                    <p className='text-4xl xl:text-5xl font-bold opacity-60 mb-4  '>Earphone</p>
+                                    <p className='text-4xl xl:text-5xl font-bold opacity-60 mb-4'>Earphone</p>
                                    <Button text="Browse" bgColor="bg-primary" textColor="text-white"/>
                                 </div>
                             </div>
                             <img src={Image1} alt="earphone" className='w-[320px] absolute bottom-0' />
                         </div>
                         {/* second col*/}
-                        <div className='py-10 pl-5 bg-gradient-to-b from-black 90 to-brandYellow/60 text-white rounded-3xl relative h-[320px] flex items-end '>
+                        <div className='py-10 pl-5 bg-gradient-to-b from-black 90 to-brandsecondary/60 text-white rounded-3xl relative h-[320px] flex items-end '>
                             <div>
                                 <div>
                                     <p className='mb-4'>Enjoy</p>
                                     <p className='mb-[2px] text-gray-400 '>with</p>
-                                    <p className='text-4xl xl:text-5xl font-bold opacity-60 mb-4  '>Earphone</p>
+                                    <p className='text-4xl xl:text-5xl font-bold opacity-60 mb-4'>Watch</p>
                                    <Button text="Browse" bgColor="bg-primary" textColor="text-white"/>
                                 </div>
                             </div>
                             <img src={Image2} alt="earphone" className='w-[320px]  absolute -right-20 lg:top-[110px]' />
                         </div>
                         {/* third col*/}
-                        <div className=' col-span-2 py-10 pl-5 bg-gradient-to-b from-black 90 to-primary/60 relative rounded-3xl text-white h-[320px] flex items-end'>
+                        <div className='col-span-2 py-10 pl-5 bg-gradient-to-b from-black 90 to-primary/60 relative rounded-3xl text-white h-[320px] flex items-end'>
                             <div>
-                                <div>
+                                <div className=' space-y-2 mb-4'>
                                     <p className='mb-4'>Enjoy</p>
                                     <p className='mb-[2px] text-gray-300'>With</p>
                                     <p className='text-3xl lg:text-5xl font-bold opacity-60 mb-4'>Laptop</p>
                                     <Button text="browse" textColor="text-white" bgColor="bg-primary"/>
                                 </div>
                             </div>
-                            <img src={Image3}alt="maclap" className=' w-[400px] absolute top-1/2 -translate-y-1/2 -right-0' />
+                            <img src={Image3}alt="macbooklap" className=' w-[300px] absolute top-1/2 -translate-y-1/2 -right-0' />
                         </div>
                     </div>
         </div>
     </div>  
   )
 }
+
