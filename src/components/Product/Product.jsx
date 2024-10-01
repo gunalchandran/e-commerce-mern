812c12b1f65da5497cf7cfa653346c0470d4e1f6
Gunal <gunalc.22cse@kongu.edu>
2024-10-01 20:50:24 +0530

    2

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Headering from '../shared/Headering.jsx'
 import ProductCard from'../Product/ProductCard.jsx'
-import Img1 from '../../assets/Headphone1.jpg'
-import Img2 from'../../assets/Headphone.png' 
 import Img3 from'../../assets/watch.png' 
 import Img4 from'../../assets/speaker.png' 
 import Img5 from'../../assets/gaming.png' 
@@ -12,20 +10,20 @@ import Img8 from '../../assets/dumstone.jpeg'
 import Img9 from '../../assets/8inch.jpeg'
 import Img10 from '../../assets/Apple_watch.jpeg'
 const productsData=[
-  {
-    id:1,
-    img:Img10,
-    title:"Apple Watch",
-    price:"₹ 20000",
-    aosDelay:"0",
-  },
-  {
-    id:1,
-    img:Img9,
-    title:"8-Inch-wallspeaker",
-    price:"₹ 2500",
-    aosDelay:"0",
-  },
+  // {
+  //   id:1,
+  //   img:Img10,
+  //   title:"Apple Watch",
+  //   price:"₹ 20000",
+  //   aosDelay:"0",
+  // },
+  // {
+  //   id:1,
+  //   img:Img9,
+  //   title:"8-Inch-wallspeaker",
+  //   price:"₹ 2500",
+  //   aosDelay:"0",
+  // },
   {
     id:1,
     img:Img3,
@@ -47,28 +45,13 @@ const productsData=[
     price:" ₹ 7000",
     aosDelay:"0",
   },
-  {
-    id:1,
-    img:Img7,
-    title:"Boat Headphone",
-    price:" ₹ 1000",
-    aosDelay:"0",
-  },
-  {
-    id:1,
-    img:Img8,
-    title:"Boat Headphone",
-    price:" ₹ 10000",
-    aosDelay:"0",
-  },
-  {
+   {
     id:1,
     img:Img6,
-    title:"Boat Airpodes",
-    price:" ₹ 1700",
+    title:"Vedio-Gaming ",
+    price:" ₹ 7000",
     aosDelay:"0",
-  },
-  
+  }
 ]
 const Product = () => {
   return (
